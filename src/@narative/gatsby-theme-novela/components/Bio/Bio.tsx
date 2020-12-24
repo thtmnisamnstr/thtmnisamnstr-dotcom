50bcd6e578bbc64030bcab9b182fa19a6c5d0dd9
Gavin Johnson <gavinj1984@gmail.com>
2020-12-23 19:05:05 -0800

    fixed the featured author short bio

--- src/@narative/gatsby-theme-novela/components/Bio/Bio.tsx
+++ src/@narative/gatsby-theme-novela/components/Bio/Bio.tsx
@@ -12,7 +12,7 @@ const Bio: React.FC<IAuthor> = ({ author }) => {
         as={author.authorsPage ? Link : 'div'}
         to={author.slug}
         data-a11y="false"
-        aria-label="Author's bio"
+        aria-label="Author's short bio"
       >
         <BioAvatarInner>
           <RoundedImage src={author.avatar.medium} />
